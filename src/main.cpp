e39e1f32c29e1825d3e8e21d6ec34cc6d1014760
br0ce <beck@informatik.uni-bonn.de>
2016-04-29 12:20:35 +0200

    formating

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 /** @file main.cpp
- * 
+ *
  * @brief Small gui_wrapper for eigen.
- * 
+ *
  * Copyright (C) 2016  @author Niklas Beck, beck@informatik.uni-bonn.de
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * 
+ *
  */
 
 #include <QApplication>
@@ -27,8 +27,8 @@
 
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
-    main_win main_win;
-    main_win.show();
-    return app.exec();
+  QApplication app(argc, argv);
+  main_win main_win;
+  main_win.show();
+  return app.exec();
 }
